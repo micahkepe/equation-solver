d357ea243da9247ad0b20fe67252427b8c039846
MicahKepe <mik3@rice.edu>
2023-05-25 14:52:23 -0700

    deleting dataset loading code, look for another dataset/ method

--- dataprep.py
+++ dataprep.py
@@ -11,14 +11,5 @@ import xml.etree.ElementTree as ET
 root_directory = '/Users/micahkepe/Downloads/archive'
 data = {}
 
-for folder_name in os.listdir(root_directory):
-    folder_path = os.path.join(root_directory, folder_name)
-    if os.path.isdir(folder_path):
-        folder_data = []
-        for file_name in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, file_name)
-            if file_name.endswith('.inkml'):
-                tree = ET.parse(file_path)
-                root = tree.getroot()
-                traces = []
+
                 
